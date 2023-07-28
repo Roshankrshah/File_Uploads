1c276d3a7f37e6168f97287b7bada6f4c6c508c4
Roshan <shahroshan0720@gmail.com>
2023-07-28 18:21:23 +0530

    add cloud db

--- app.js
+++ app.js
@@ -3,14 +3,26 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+const connectDB = require('./db/connect');
+
 app.use(express.json());
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("Hello");
 })
 
+
 PORT = process.env.PORT || 4001
 
-app.listen(PORT,()=>{
-    console.log(`Server listening at http://localhost:${PORT}`);
-});
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(PORT, () => {
+            console.log(`Server listening at http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
